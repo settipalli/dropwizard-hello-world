43adcb1f363d03c8407b819f0a014aae0e255719
Santhoshkumar Settipalli <santhosh.settipalli@gmail.com>
2017-12-05 22:18:16 +0000

    Updated configuration.

--- src/main/java/com/dp/HelloWorldConfiguration.java
+++ src/main/java/com/dp/HelloWorldConfiguration.java
@@ -3,8 +3,33 @@ package com.dp;
 import io.dropwizard.Configuration;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import org.hibernate.validator.constraints.*;
+
 import javax.validation.constraints.*;
 
 public class HelloWorldConfiguration extends Configuration {
-    // TODO: implement service configuration
+    @NotNull
+    private String template;
+
+    @NotNull
+    private String defaultName = "Stranger";
+
+    @JsonProperty
+    public String getTemplate() {
+        return template;
+    }
+
+    @JsonProperty
+    public void setTemplate(String template) {
+        this.template = template;
+    }
+
+    @JsonProperty
+    public String getDefaultName() {
+        return defaultName;
+    }
+
+    @JsonProperty
+    public void setDefaultName(String defaultName) {
+        this.defaultName = defaultName;
+    }
 }
